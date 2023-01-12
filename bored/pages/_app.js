c449ce5748d2a5b702f1072a68b248b55f87a7fc
Roddur Dasgupta <roddurd@gmail.com>
2023-01-11 16:36:02 -0800

    bulletin bored title everywhere

--- bored/pages/_app.js
+++ bored/pages/_app.js
@@ -2,12 +2,18 @@ import Layout from "../components/layout";
 import "../styles/globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <ToastContainer limit={1} />
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Bulletin Bored</title>
+      </Head>
+      <Layout>
+        <ToastContainer limit={1} />
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
